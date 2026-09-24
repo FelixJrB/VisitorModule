79e78261445bd3474e320c4da3509e1d0d04e98d
Felix Berglund <fb222rs@student.lnu.se>
2026-09-24 11:56:27 +0200

    feat: add export on DeviceType and OperatingSystem

--- src/Visitor.ts
+++ src/Visitor.ts
@@ -6,8 +6,8 @@ type VisitorData = {
   operatingSystem: OperatingSystem
 }
 
-type DeviceType = 'Desktop' | 'Mobile' | 'Tablet' | 'Tv' | 'Unknown'
-type OperatingSystem = 'Windows' | 'MacOs' | 'IOS' | 'Android' | 'Linux' | 'Unknown'
+export type DeviceType = 'Desktop' | 'Mobile' | 'Tablet' | 'Tv' | 'Unknown'
+export type OperatingSystem = 'Windows' | 'MacOs' | 'IOS' | 'Android' | 'Linux' | 'Unknown'
 
 /**
  * A class representing a visitor with various properties such as unique ID, IP address, location, device type, and operating system.
